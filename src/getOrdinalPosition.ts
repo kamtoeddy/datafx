a3dc1266a585d60f7404cc78563600ac7ff4ebc0
kamtoeddy <lollipopkalog@gmail.com>
2022-06-21 17:33:04 +0100

    made getOrdinalPosition to consider only the integer part of the incoming number

--- src/getOrdinalPosition.ts
+++ src/getOrdinalPosition.ts
@@ -7,5 +7,5 @@ export const getOrdinalPosition = (value: number): number | string => {
 
   if (value === 0 || (value >= 4 && value <= 20)) return "th";
 
-  return getOrdinalPosition(Number(value % 10));
+  return getOrdinalPosition(parseInt(`${value % 10}`));
 };
