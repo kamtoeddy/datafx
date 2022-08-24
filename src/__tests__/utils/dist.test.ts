55e44480834512db372b38d0c6d33822edfddbf5
kamtoeddy <lollipopkalog@gmail.com>
2022-08-24 18:29:38 +0100

    updated tests for utils

---
+++ src/__tests__/utils/dist.test.ts
@@ -0,0 +1,8 @@
+import { capitalise, isEqual, useIf } from "../../../dist";
+import { capitalise_Tests, isEqual_Tests, useIf_Tests } from ".";
+
+capitalise_Tests({ capitalise });
+
+isEqual_Tests({ isEqual });
+
+useIf_Tests({ useIf });
