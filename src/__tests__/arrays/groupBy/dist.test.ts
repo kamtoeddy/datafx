de08e1f8f374ce181b08191181b9bda8746fe035
kamtoeddy <lollipopkalog@gmail.com>
2022-08-25 17:24:13 +0100

    updated tests for groupBy

---
+++ src/__tests__/arrays/groupBy/dist.test.ts
@@ -0,0 +1,4 @@
+import { groupBy_Tests } from ".";
+import { groupBy } from "../../../../dist";
+
+groupBy_Tests({ groupBy });
---
+++ src/__tests__/arrays/groupBy/index.test.ts
@@ -0,0 +1,4 @@
+import { groupBy } from "../../../";
+import { groupBy_Tests } from ".";
+
+groupBy_Tests({ groupBy });
