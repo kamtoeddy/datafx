0ecc2d8d366853b6cfa625618000cb3cb121fee7
kamtoeddy <lollipopkalog@gmail.com>
2022-08-29 20:20:11 +0100

    did cleanups

--- src/Translator.ts
+++ src/Translator.ts
@@ -1,43 +1,35 @@
 import { ObjectType } from "./interfaces";
 import { getDeepValue } from "./objects";
 
-interface Translations {
-  [locale: string]: ObjectType;
-}
+type Translations = Record<string, ObjectType>;
 
 export class Translator {
-  [key: string]: any;
-
   fallback: string = "";
   locale: string = "";
   locales: string[] = [];
-  private translations: Translations = {};
 
-  constructor(translations: Translations, fallback?: string) {
+  constructor(private translations: Translations, fallback: string) {
     this.translations = translations;
 
-    const locales = Object.keys(this.translations);
-    this.locales = locales;
+    this.locales = Object.keys(this.translations);
 
-    if (!fallback || !this.isValidLocale(fallback)) fallback = locales[0];
+    if (!fallback || !this.isValidLocale(fallback)) fallback = this.locales[0];
 
     this.fallback = fallback;
 
     this.setLocale(fallback);
   }
 
-  private isValidLocale = (locale: any) => {
-    return this.locales.includes(locale);
-  };
+  private isValidLocale = (locale: any) => this.locales.includes(locale);
 
   private getAlternative(key: string, depth: number) {
     const keys = key.split(".");
 
-    return keys[keys.length - depth];
+    return key.split(".")[keys.length - depth];
   }
 
   setLocale = (locale: string) => {
-    if (!this.locales.includes(locale)) return;
+    if (!this.isValidLocale(locale)) return;
     this.locale = locale;
 
     return this.locale;
