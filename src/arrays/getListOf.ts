4d3eb7107d5d858fdd73b2f3612d72498bf7dbe7
kamtoeddy <lollipopkalog@gmail.com>
2022-09-11 20:22:14 +0100

    updated typing for getListOf

--- src/arrays/getListOf.ts
+++ src/arrays/getListOf.ts
@@ -1,11 +1,11 @@
-import { ObjectType } from "../interfaces";
+import { NestedKeyOf, ObjectType } from "../interfaces";
 import { getDeepValue } from "../objects";
 
 type Options = { unique?: boolean };
 
-export const getListOf = <T = any>(
-  list: any[],
-  key: string,
+export const getListOf = <T>(
+  list: T[],
+  key: NestedKeyOf<T>,
   { unique }: Options = { unique: false }
 ): T[] => {
   const _list = list.map((dt) => getDeepValue(dt as ObjectType, key));
