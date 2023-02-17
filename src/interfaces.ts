51714d5657bc92de2a8396b6e87276571ba3b078
kamtoeddy <kamtoeddytagne@gmail.com>
2023-02-17 10:36:30 +0100

    fix: fixed typings errors with typescript with 'groupBy' util

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,12 @@
 export type ObjectType = Record<number | string, any>;
 export type StringKey<T> = Extract<keyof T, string>;
-type KeyOf<T> = keyof T & (string | number);
 
 export type NestedKeyOf<T> = T extends never
   ? ""
   : {
-      [Key in KeyOf<T>]: T[Key] extends ObjectType
-        ? `${Key}` | `${Key}.${NestedKeyOf<T[Key]>}`
-        : `${Key}`;
-    }[KeyOf<T>];
+      [K in StringKey<T>]: T[K] extends Function
+        ? never
+        : T[K] extends Array<any>
+        ? `${K}.${NestedKeyOf<T[K]>}`
+        : `${K}` | `${K}.${NestedKeyOf<T[K]>}`;
+    }[StringKey<T>];
